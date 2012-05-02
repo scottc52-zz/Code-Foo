dd0a485cfff8a3543c4729efd0d73bf80b0397da
Scott Chung <scottc52@gmail.com>
2012-05-02 05:03:56 -0700

    better license plate fix

--- calculateLicensePlates.js
+++ calculateLicensePlates.js
@@ -8,31 +8,25 @@ function calculateLicensePlates(){
 		if(population==0){
 			out = "Population: 0<br />Pattern: 0 Numbers, 0 Letters<br />Total Plates: 0<br />Excess: 0";
 		}else{
-			var numbers = 1;
-			var numPlates = 10;
-			while(population>numPlates){
-				numbers = numbers + 1;
-				numPlates = numPlates*10;
-			}
+			var numbers = 0;
 			var letters = 0;
-			var numMax = numbers;
-			var tempNumPlates;
-			var tempLetters;
-			var tempNumbers;
-			
-			for(tempNumbers = 0; tempNumbers<=numMax; tempNumbers = tempNumbers + 1){
-				tempNumPlates = Math.pow(10, tempNumbers);
+			var numPlates = population*5000; // this just needs to be a large enough number that it'll be overwritten.
+			var tempNumbers = 0;
+			var tempLetters = 1; // can't be 0 because then while loop won't start
+			var tempNumPlates = 1;
+			while(tempLetters>0){
 				tempLetters = 0;
-				
-				while(population>tempNumPlates){
+				tempNumPlates = Math.pow(10, tempNumbers);
+				while(tempNumPlates<population){
 					tempLetters = tempLetters + 1;
 					tempNumPlates = tempNumPlates * 26;
 				}
 				if(tempNumPlates<numPlates){
 					numPlates = tempNumPlates;
-					letters = tempLetters;
 					numbers = tempNumbers;
+					letters = tempLetters;
 				}
+				tempNumbers = tempNumbers + 1;
 			}
 			out = "Population: " + population + "<br />Pattern: " + numbers + " Numbers, " + letters +" Letters<br />Total Plates: "+ numPlates + "<br />Excess: " + (numPlates-population);
 		}
